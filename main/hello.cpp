1d2ac2e3d485c6a05feaa81be9222e6d92ba60fc
lagantg19 <77987396+lagantg19@users.noreply.github.com>
2023-02-15 18:18:41 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World,this is PES1UG20CS555 after change \n";
+  cout << "Hello, World,this is PES1UG20CS555 after 1change \n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
